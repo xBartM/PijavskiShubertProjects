14a065c71602c42f3a72b6410a38b91f78da1ecb
xBartM <mielczarek.cs@gmail.com>
2019-12-01 20:18:11 +0100

    It's ugly but it works

--- Makefile
+++ Makefile
@@ -1,25 +1,32 @@
-CC = gcc
+CC = gcc #-mavx2
 CFLAGS = -Wall -pedantic -Wextra
 LIBS = -lm
 
-all: PlainC/main
+all: PlainC/main #AVX2/main
 
 	
 PlainC/main: PlainC/o/main.o testfunctions/o/testfunctions.o
 	${CC} PlainC/o/main.o testfunctions/o/testfunctions.o -o  PlainC/main ${CFLAGS} ${LIBS}
 
+# AVX2/main: AVX2/o/main.o
+	# ${CC} AVX2/o/main.o -o  AVX2/main ${CFLAGS} ${LIBS}
+
 
 PlainC/o/main.o: PlainC/main.c 
 	${CC} PlainC/main.c -o PlainC/o/main.o -c ${CFLAGS}
+
+# AVX2/o/main.o: AVX2/main.c 
+	# ${CC} AVX2/main.c -o AVX2/o/main.o -c ${CFLAGS}
+
 
 testfunctions/o/testfunctions.o: testfunctions/testfunctions.c testfunctions/testfunctions.h
 	${CC} testfunctions/testfunctions.c -o testfunctions/o/testfunctions.o -c ${CFLAGS}
 
 
 clean_intermediate:
-	rm -f PlainC/o/*.o testfunctions/o/*.o
+	rm -f PlainC/o/*.o AVX2/o/*.o testfunctions/o/*.o
 
 clean_bin:
-	rm -f PlainC/main
+	rm -f PlainC/main AVX2/main
 
 clean: clean_intermediate clean_bin
--- PlainC/main.c
+++ PlainC/main.c
@@ -22,6 +22,8 @@
 // L4 = 17.3365;
 
 #include <stdio.h>
+//#include <time.h>
+#include <sys/time.h>
 #include <math.h>
 
 #include "../testfunctions/testfunctions.h"
@@ -32,44 +34,162 @@
 // const double L4 = __L4;
 
 
-const double epsilon = 0.0001; // we'll see
+const double epsilon = 0.0000001; // we'll see
 double global_fmin = INFINITY;
  
-double alg (double, double);
+double alg1 (double, double);
+double alg2 (double, double);
+double alg3 (double, double);
+double alg4 (double, double);
  
 int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv;
+    struct timeval t1, t2;
+    double elapsed_time;
+    double ret;
 
-    // double a = __A1;
-    // double b = __B1;
+    double a1 = __A1;
+    double b1 = __B1;
 
-    // double a = __A2;
-    // double b = __B2;
+    global_fmin = INFINITY;
+    gettimeofday(&t1, NULL);
+    ret = alg1(a1,b1);
+    gettimeofday(&t2, NULL);
+    elapsed_time = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+    elapsed_time += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
 
-    double a = __A3;
-    double b = __B3;
+    printf("f1 time=%.3fms\nx=%f\nf(x)=%f\n\n", elapsed_time, ret, f1(ret));
 
-    // double a = __A4;
-    // double b = __B4;
+    double a2 = __A2;
+    double b2 = __B2;
     
-    double ret = alg(a,b);
-    printf("x=%f\nf(x)=%f\n", ret, f3(ret));
+    global_fmin = INFINITY;
+    gettimeofday(&t1, NULL);
+    ret = alg2(a2,b2);
+    gettimeofday(&t2, NULL);
+    elapsed_time = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+    elapsed_time += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
 
-   
-    printf("Hello, World!\n");
+    printf("f2 time=%.3fms\nx=%f\nf(x)=%f\n\n", elapsed_time, ret, f2(ret));
+
+    double a3 = __A3;
+    double b3 = __B3;
+
+    global_fmin = INFINITY;
+    gettimeofday(&t1, NULL);
+    ret = alg3(a3,b3);
+    gettimeofday(&t2, NULL);
+    elapsed_time = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+    elapsed_time += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+
+    printf("f3 time=%.3fms\nx=%f\nf(x)=%f\n\n", elapsed_time, ret, f3(ret));
+
+    double a4 = __A4;
+    double b4 = __B4;
+    
+    global_fmin = INFINITY; 
+    gettimeofday(&t1, NULL);
+    ret = alg4(a4,b4);
+    gettimeofday(&t2, NULL);
+    elapsed_time = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+    elapsed_time += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+
+    printf("f4 time=%.3fms\nx=%f\nf(x)=%f\n\n", elapsed_time, ret, f4(ret));
     return 0;
 }
 
 
-double alg (double a, double b)
+
+
+double alg1 (double a, double b)
+{
+    double xmin = h1min (a, b);  //  x value for which h(x) is min 
+    double hmin = h1(xmin, a, b);  //  min(h(x))
+    double to_return1, to_return2; // temp
+    double f_val;   // temp2 xd
+    // printf("a=%f, b=%f, xmin=%f, hmin=%f, global_fmin=%f, fabs=%f\n", a, b, xmin, hmin, global_fmin, fabs(a-b));
+
+    // this is important - global_fmin should be a vector, and.. W8 xd - just update global_fmin according to f_val :V - so much faster O:)
+    if (hmin > global_fmin)
+        return NAN;
+    else if (fabs(a - b) < epsilon)
+    {
+        f_val = f1(xmin);
+        if (global_fmin > f_val)
+            global_fmin = f_val; // change global fmin -- it should have mutex on it later
+        return xmin;
+    }
+    else
+    {
+        f_val = f1(xmin);
+        if (global_fmin > f_val)
+            global_fmin = f_val; // change global fmin -- it should have mutex on it later
+        
+        to_return1 = alg1 (a, xmin);
+        to_return2 = alg1 (xmin, b);
+
+        if (isnan(to_return1))
+            return to_return2;
+        else if (isnan(to_return2))
+            return to_return1;
+        else if (f1(to_return1) < f1(to_return2))
+            return to_return1;
+        else
+            return to_return2;
+        
+    }
+    
+}
+
+double alg2 (double a, double b)
+{
+    double xmin = h2min (a, b);  //  x value for which h(x) is min 
+    double hmin = h2(xmin, a, b);  //  min(h(x))
+    double to_return1, to_return2; // temp
+    double f_val;   // temp2 xd
+    // printf("a=%f, b=%f, xmin=%f, hmin=%f, global_fmin=%f, fabs=%f\n", a, b, xmin, hmin, global_fmin, fabs(a-b));
+
+    // this is important - global_fmin should be a vector, and.. W8 xd - just update global_fmin according to f_val :V - so much faster O:)
+    if (hmin > global_fmin)
+        return NAN;
+    else if (fabs(a - b) < epsilon)
+    {
+        f_val = f2(xmin);
+        if (global_fmin > f_val)
+            global_fmin = f_val; // change global fmin -- it should have mutex on it later
+        return xmin;
+    }
+    else
+    {
+        f_val = f2(xmin);
+        if (global_fmin > f_val)
+            global_fmin = f_val; // change global fmin -- it should have mutex on it later
+        
+        to_return1 = alg2 (a, xmin);
+        to_return2 = alg2 (xmin, b);
+
+        if (isnan(to_return1))
+            return to_return2;
+        else if (isnan(to_return2))
+            return to_return1;
+        else if (f2(to_return1) < f2(to_return2))
+            return to_return1;
+        else
+            return to_return2;
+        
+    }
+    
+}
+
+double alg3 (double a, double b)
 {
     double xmin = h3min (a, b);  //  x value for which h(x) is min 
     double hmin = h3(xmin, a, b);  //  min(h(x))
     double to_return1, to_return2; // temp
     double f_val;   // temp2 xd
-    printf("a=%f, b=%f, xmin=%f, hmin=%f, global_fmin=%f, fabs=%f\n", a, b, xmin, hmin, global_fmin, fabs(a-b));
+    // printf("a=%f, b=%f, xmin=%f, hmin=%f, global_fmin=%f, fabs=%f\n", a, b, xmin, hmin, global_fmin, fabs(a-b));
 
     // this is important - global_fmin should be a vector, and.. W8 xd - just update global_fmin according to f_val :V - so much faster O:)
     if (hmin > global_fmin)
@@ -87,14 +207,54 @@ double alg (double a, double b)
         if (global_fmin > f_val)
             global_fmin = f_val; // change global fmin -- it should have mutex on it later
         
-        to_return1 = alg (a, xmin);
-        to_return2 = alg (xmin, b);
+        to_return1 = alg3 (a, xmin);
+        to_return2 = alg3 (xmin, b);
 
         if (isnan(to_return1))
             return to_return2;
         else if (isnan(to_return2))
             return to_return1;
         else if (f3(to_return1) < f3(to_return2))
+            return to_return1;
+        else
+            return to_return2;
+        
+    }
+    
+}
+
+double alg4 (double a, double b)
+{
+    double xmin = h4min (a, b);  //  x value for which h(x) is min 
+    double hmin = h4(xmin, a, b);  //  min(h(x))
+    double to_return1, to_return2; // temp
+    double f_val;   // temp2 xd
+    // printf("a=%f, b=%f, xmin=%f, hmin=%f, global_fmin=%f, fabs=%f\n", a, b, xmin, hmin, global_fmin, fabs(a-b));
+
+    // this is important - global_fmin should be a vector, and.. W8 xd - just update global_fmin according to f_val :V - so much faster O:)
+    if (hmin > global_fmin)
+        return NAN;
+    else if (fabs(a - b) < epsilon)
+    {
+        f_val = f4(xmin);
+        if (global_fmin > f_val)
+            global_fmin = f_val; // change global fmin -- it should have mutex on it later
+        return xmin;
+    }
+    else
+    {
+        f_val = f4(xmin);
+        if (global_fmin > f_val)
+            global_fmin = f_val; // change global fmin -- it should have mutex on it later
+        
+        to_return1 = alg4 (a, xmin);
+        to_return2 = alg4 (xmin, b);
+
+        if (isnan(to_return1))
+            return to_return2;
+        else if (isnan(to_return2))
+            return to_return1;
+        else if (f4(to_return1) < f4(to_return2))
             return to_return1;
         else
             return to_return2;
